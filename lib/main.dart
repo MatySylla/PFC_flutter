2906bd85f604810b85bfddb919912288954a1ca8
boussomdn <mdiarra.ndiaye1@univ-thies.sn>
2024-03-29 22:34:46 +0000

    modification titre

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'maty mame',
+      title: 'maty mame Diarra',
       theme: ThemeData(
         // This is the theme of your application.
         //
